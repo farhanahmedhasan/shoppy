a8d6fd596932f831bc491e6b94697ca7874bcfe2
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2024-01-01 23:09:20 +0600

    wip

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Banner() {
+    return (
+        <div className="banner">
+            <h1>Welcome to Shoppy!</h1>
+            <p>Discover the best deals on your favorite products.</p>
+        </div>
+    );
+}
+
+export default Banner;
